a368b35c5a5db240fccb21120e5227e7ec1b004f
Guru9045 <141578846+Guru9045@users.noreply.github.com>
2023-08-14 03:20:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter First NAme ");
+            String s1= Console.ReadLine();
+            
+
+            Console.WriteLine("Enter Second NAme ");
+            String s2 = Console.ReadLine();
+           
+            Console.WriteLine("Enter Third NAme ");
+            String s3 = Console.ReadLine();
+           
+            Console.WriteLine("Enter 4th NAme ");
+            String s4 = Console.ReadLine();
+            
+            Console.WriteLine("Enter 5th NAme ");
+            String s5 = Console.ReadLine();
+           
+            Console.WriteLine("Enter 6th NAme ");
+            String s6 = Console.ReadLine();
+            
+            Console.WriteLine("Enter 7th NAme ");
+            String s7 = Console.ReadLine();
+            
+            Console.WriteLine("Enter 8th NAme ");
+            String s8 = Console.ReadLine();
+
+            Console.WriteLine("\n Team members:-");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s4);
+            Console.WriteLine(s3);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6); 
+            Console.WriteLine(s7);
+            Console.WriteLine(s8);
+
+            Console.ReadLine();
+
+        }
+    }
+}
